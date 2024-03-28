4dde5c77c448bda8ad554563c14cd4b3335df7aa
shon0921 <0921shon@naver.com>
2024-03-28 23:29:16 +0900

    10809번 알파벳 찾기

---
+++ src/Other/Ch8.java
@@ -0,0 +1,17 @@
+package Other;
+
+import java.util.Scanner;
+
+public class Ch8 {
+    public static void main(String[] args) {
+        String y;
+        Scanner str = new Scanner(System.in);
+
+        System.out.println("영어 입력");
+        y = str.nextLine();
+        for(char a= 'a'; a<='z'; a++)
+        {
+            System.out.println(y.indexOf(a)+" ");
+        }
+    }
+}
